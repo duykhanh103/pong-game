20c03e48c93b39ee9ef1144f587965edf2af8389
Khanh Duy <86542640+duykhanh103@users.noreply.github.com>
2021-06-28 15:34:08 +0700

    Update : Add instructions!

--- Pong.py
+++ Pong.py
@@ -4,39 +4,50 @@ pygame.init()
 pygame.font.init() 
 
 
-
+# Set display, width : 800px, height : 600px
 Screen = pygame.display.set_mode((800, 600))
+# Set title or caption or whatever
 pygame.display.set_caption("Pong")
+# Set clock for control FPS.
 FPS = pygame.time.Clock()
+
+#I set running = True just for creating game loop
 running = True
 
+# Our screen color - Grey, this is RGB color.
 Screen.fill((64, 64, 64))
 
 # Draw some rectangles!
-myfont = pygame.font.SysFont('Arial', 20)
+myfont = pygame.font.SysFont('Arial', 15)
 
 
-
+# The basic set-up for Pong game : Ball and 2 players.
+# pygame.Rect(x, y, width, height) - x, y is Rect position, You should check how Pygame works to have a better overview!
 Ball = pygame.Rect(375, 275, 20, 20)
 Player1 = pygame.Rect(10, 160, 10, 100)
 Player2 = pygame.Rect(780, 160, 10, 100)
+
 Ball_x = 5
 Ball_y = 5
+
 Player1Score = 0
 Player2Score = 0
 
 def Game_restart():
+    # I just simply reset the Ball position (exactly the same)
     Ball.x = 375
     Ball.y = 275
 
 def BallAnimation():
 
-
+    # These variables are local variables, which mean you can only use it within this function, to use it outside the function,
+    # Just turn it into global variables (Learn more about Scope).
     global Ball_x, Ball_y, Player1Score, Player2Score
     Ball.x += Ball_x
     Ball.y += Ball_y
 
     if Ball.top <= 0 or Ball.bottom >= 600:
+        # When the ball hits the top and bottom wall, it bounces.
         Ball_y *= -1
 
 
@@ -47,12 +58,13 @@ def BallAnimation():
             Player1Score += 1
         Game_restart()
 
-
+    # Check for collisons!
     if Ball.colliderect(Player1) or Ball.colliderect(Player2):
         Ball_x *= -1
 
 while running:
 
+    # This for loop keeps window visible
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -76,16 +88,22 @@ while running:
 
     Screen.fill((64, 64, 64))
 
+    # My above function!
     BallAnimation()
 
+    # We have to draw some rects we have created above!
     pygame.draw.rect(Screen, (255, 255, 255), Player1)
     pygame.draw.rect(Screen, (255, 255, 255), Player2)
     pygame.draw.ellipse(Screen, (255, 255, 255), Ball)
     
+
     Screen.blit(textsurface,(0,0))
     Screen.blit(textsurface2,(600,0))
 
     pygame.display.update()
+
+    # From the pygame.time.Clock(), just to make FPS
     FPS.tick(60)
     pygame.display.flip()
+
 
